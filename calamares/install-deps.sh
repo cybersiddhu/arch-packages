98cca8d9bfdc8bd01b34215d9145dc6b211d7e03
Siddhartha Basu <biosidd@gmail.com>
2020-11-09 22:37:43 -0600

    feat: add calamares PKGBUILD

---
+++ calamares/install-deps.sh
@@ -0,0 +1,5 @@
+pacman -S --needed --noconfirm kconfig kcoreaddons kiconthemes ki18n kio solid yaml-cpp kpmcore \
+	boost-libs hwinfo qt5-svg polkit-qt5 gtk-update-icon-cache plasma-framework \
+	qt5-xmlpatterns squashfs-tools extra-cmake-modules qt5-tools \
+	qt5-translations git boost
+yay -S mkinitcpio-openswap ckbcomp --noconfirm
